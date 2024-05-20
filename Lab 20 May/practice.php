f6e8a1088e5ca1e3befea826bb9650ef5dcbd067
Rii <129301857+RimjhimD@users.noreply.github.com>
2024-05-20 11:01:21 +0600

    PHP Practice

---
+++ Lab 20 May/practice.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>PHP Practice</title>
+</head>
+<body>
+    <center>
+        <?php
+        $name = 'Rii';
+        $age = 25;
+        $name = 'Eri';
+        $age = 19;
+        echo 'Name is ',$name.'';
+        
+        echo 'Age is ',$age.'';
+        ?>
+
+    </center>
+</body>
+</html>
